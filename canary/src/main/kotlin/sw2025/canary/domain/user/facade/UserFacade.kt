49c0107c6e8e7b037736a146624e9aa96ffbb8e4
dohwi <dohwe0978@gmail.com>
2025-10-15 14:34:43 +0900

    feat : findUserByIdOrThrow

--- canary/src/main/kotlin/sw2025/canary/domain/user/facade/UserFacade.kt
+++ canary/src/main/kotlin/sw2025/canary/domain/user/facade/UserFacade.kt
@@ -2,6 +2,7 @@ package sw2025.canary.domain.user.facade
 
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
+import sw2025.canary.domain.user.exception.UserNotFoundException
 import sw2025.canary.domain.user.persistence.repository.UserRepository
 
 @Component
@@ -9,5 +10,5 @@ class UserFacade(
     private val userRepository: UserRepository
 ) {
     fun currentUser() = userRepository.findById((SecurityContextHolder.getContext().authentication.name).toLong())
-
+    fun findUserByIdOrThrow(userId: Long) = userRepository.findById(userId).orElseThrow{UserNotFoundException}
 }
