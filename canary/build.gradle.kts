89dd724edc99f6c6be11ad852584d8a087ecd3eb
dohwi <dohwe0978@gmail.com>
2025-10-15 15:53:00 +0900

    feat : jwt 0.12.6으로 버전 업

--- canary/build.gradle.kts
+++ canary/build.gradle.kts
@@ -47,7 +47,10 @@ dependencies {
 	//redis
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	//jwt
-	implementation("io.jsonwebtoken:jjwt:0.9.1")
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6") // JSON 직렬화
+
 }
 
 kotlin {
