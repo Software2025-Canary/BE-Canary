b0da43fa3691d0109be637a1b64256e4e3a00980
dohwi <dohwe0978@gmail.com>
2025-10-15 13:18:36 +0900

    refactor : lexical correction repository

--- canary/src/main/kotlin/sw2025/canary/domain/lexical_correction/repository/LexicalCorrectionRepository.kt
+++ canary/src/main/kotlin/sw2025/canary/domain/lexical_correction/repository/LexicalCorrectionRepository.kt
@@ -3,5 +3,5 @@ package sw2025.canary.domain.lexical_correction.repository
 import org.springframework.data.repository.CrudRepository
 import sw2025.canary.domain.lexical_correction.domain.LexicalCorrection
 
-interface LexicalCorrection: CrudRepository<LexicalCorrection, Long> {
+interface LexicalCorrectionRepository: CrudRepository<LexicalCorrection, Long> {
 }
