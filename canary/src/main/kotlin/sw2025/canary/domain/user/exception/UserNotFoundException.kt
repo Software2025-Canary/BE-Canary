bc9f869faf45e435fe4d80bef3005fe1e8730d9b
dohwi <dohwe0978@gmail.com>
2025-10-15 14:35:14 +0900

    feat : usernotfoundexception

---
+++ canary/src/main/kotlin/sw2025/canary/domain/user/exception/UserNotFoundException.kt
@@ -0,0 +1,8 @@
+package sw2025.canary.domain.user.exception
+
+import sw2025.canary.global.error.exception.CanaryException
+import sw2025.canary.global.error.exception.ErrorCode
+
+object UserNotFoundException: CanaryException (
+    ErrorCode.USER_NOT_FOUND
+)
