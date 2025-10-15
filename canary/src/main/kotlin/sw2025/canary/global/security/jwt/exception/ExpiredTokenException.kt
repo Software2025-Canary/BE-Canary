0fe610df4a807cd8993c8b83f615d9cb2ea535b6
dohwi <dohwe0978@gmail.com>
2025-10-15 14:18:44 +0900

    feat : exception

---
+++ canary/src/main/kotlin/sw2025/canary/global/security/jwt/exception/ExpiredTokenException.kt
@@ -0,0 +1,8 @@
+package sw2025.canary.global.security.jwt.exception
+
+import sw2025.canary.global.error.exception.CanaryException
+import sw2025.canary.global.error.exception.ErrorCode
+
+object ExpiredTokenException:CanaryException (
+    ErrorCode.EXPIRED_TOKEN,
+)
