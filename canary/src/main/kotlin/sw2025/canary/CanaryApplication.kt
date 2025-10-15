43424c4713376b3342565513181d8201fb4b837c
dohwi <dohwe0978@gmail.com>
2025-10-15 14:19:41 +0900

    feat : properties

--- canary/src/main/kotlin/sw2025/canary/CanaryApplication.kt
+++ canary/src/main/kotlin/sw2025/canary/CanaryApplication.kt
@@ -1,8 +1,10 @@
 package sw2025.canary
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class CanaryApplication
 
