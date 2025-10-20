d67a6b73ff4dfa62fc9d5374865c5f5e1a6b9782
dohwi <dohwe0978@gmail.com>
2025-10-20 09:14:45 +0900

    feat : situation entity 추가

---
+++ canary/src/main/kotlin/sw2025/canary/domain/bot/domain/Situation.kt
@@ -0,0 +1,19 @@
+package sw2025.canary.domain.bot.domain
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+
+@Entity
+class Situation(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+    @ManyToOne(cascade = [(CascadeType.REMOVE)])
+    val bot: Bot,
+    val situation: String,
+    val prompt: String,
+)
